a57005101154f694af3360d096fbdd8d8af5a5a8
Ed Younskevicius <ed@codefellows.com>
2021-09-24 15:23:35 -0700

    Initial Data Structures project and LinkedList skeleton

---
+++ class-05/data-structures-and-algorithms/codechallenges/lib/src/main/java/codechallenges/linkedList/Node.java
@@ -0,0 +1,12 @@
+package codechallenges.linkedList;
+
+public class Node
+{
+    public int value;
+    public Node next = null;
+
+    Node(int value)
+    {
+        this.value = value;
+    }
+}
---
+++ class-05/data-structures-and-algorithms/codechallenges/lib/src/test/java/codechallenges/linkedList/LinkedListTest.java
@@ -0,0 +1,15 @@
+package codechallenges.linkedList;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LinkedListTest
+{
+    @Test void someTest()
+    {
+        LinkedList newLL = new LinkedList();
+        System.out.println(newLL);
+
+
+    }
+}
